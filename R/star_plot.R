ae1479f31acea750ae2297def853399bd532a0ef
Michael LaQuaglia <mjl4004@med.cornell.edu>
2021-10-18 19:23:27 -0400

    Star plot

---
+++ R/star_plot.R
@@ -0,0 +1,30 @@
+#' Star Plot
+#'
+#' This function produces a ggplot plot of \code{vary} versus \code{varx} with
+#' green star markers.
+#'
+#' @param df a dataframe
+#' @param varx a string name for variable x in the dataframe df
+#' @param vary a string name for variable y in the dataframe df
+#'
+#' @return A plot with star markers of \code{vary} versus \code{varx}
+#' @export
+#'
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 geom_point
+#'
+#' @examples
+#'
+#' df_test <- data.frame(x = rnorm(10), y = rnorm(10))
+#'
+#' diamond_plot(df_test)
+#'
+#'
+
+
+star_plot <- function(df, varx, vary){
+  ggplot(df, aes(x = get(varx), y = get(vary)))+
+    geom_point(shape = 11,
+               size = 5,
+               color = "green")
+}
